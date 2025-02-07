81e4af1cc1c51558bbc511e0c7e01e9e96e5bbe2
Cássio Egidio Gomes Vicente <133404306+CassioEgidio@users.noreply.github.com>
2025-02-07 08:40:04 -0300

    Update db.php

--- celfix-v3/config/db.php
+++ celfix-v3/config/db.php
@@ -1,10 +1,10 @@
  <?php
 
     $db_name = 'GrupoCelfix';
-    $db_host = 'projectt.ddns.net';
-    $db_port = '3789';
-    $db_user = 'GrupoCelfix';
-    $db_pass = 'GrupoCelfix';
+    $db_host = '';
+    $db_port = '';
+    $db_user = '';
+    $db_pass = '';
 
     $conn = new PDO("mysql:dbname=$db_name;host=$db_host;port=$db_port", $db_user, $db_pass);
     
